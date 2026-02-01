04018879cefa59b840439de14b3b6b96cdef8ceb
manjumazumdar <32399026+manjumazumdar@users.noreply.github.com>
2026-01-31 20:32:17 -0600

    Update ec.tf

--- ec.tf
+++ ec.tf
@@ -1,8 +1,4 @@
-provider "aws" {
-    region = "us-east-1"
-}
-
-resource "aws_instance "ec2" {
-    instance_type = "t2.micro"
-    ami = "ami-0bb84b8ffd87024d8"
+resource "aws_instance" "ec2" {
+  ami           = "ami-0bb84b8ffd87024d8"
+  instance_type = "t2.micro"
 }
